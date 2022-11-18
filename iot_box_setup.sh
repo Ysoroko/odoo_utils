4b107cd46e7b1edd4926286dca3417298004a3bb
Yaroslav Soroko <36443074+Ysoroko@users.noreply.github.com>
2022-11-18 11:53:53 +0100

    Update iot_box_setup.sh

--- iot_box_setup.sh
+++ iot_box_setup.sh
@@ -1,132 +1,86 @@
 #!/bin/bash
+
+# Script arguments
 ip=${1}
 manu=${2}
 
+# Repositories paths
 ODOO_ADDONS_DIR='/home/odoo/src/odoo/addons'
 ENTERPRISE_DIR='/home/odoo/src/enterprise'
 
-# -- Colors --
-# Reset
-ENDCOLOR='\033[0m'       # Text Reset
+NOT_ENDED=1
 
-# Regular Colors
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
+# Calls echo with cenered output surrounded by "="
+function center_print() {
+    cyan=$(tput setaf 6)
+    magenta=$(tput setaf 5)
+    red=$(tput setaf 1)
+    normal=$(tput sgr0)
+    termwidth="$(tput cols)"
+    padding="$(printf '%0.1s' ={1..500})"
+    printf '\n%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "${cyan}$1${normal} ${magenta}$2${normal}" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
 
-# Bold
-BBlack='\033[1;30m'       # Black
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
-BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
-BWhite='\033[1;37m'       # White
+# Display loading spinster and green checkmard when finished
+function loading() {
+    pid=$! # Process Id of the previous running command
 
-# Underline
-UBlack='\033[4;30m'       # Black
-URed='\033[4;31m'         # Red
-UGreen='\033[4;32m'       # Green
-UYellow='\033[4;33m'      # Yellow
-UBlue='\033[4;34m'        # Blue
-UPurple='\033[4;35m'      # Purple
-UCyan='\033[4;36m'        # Cyan
-UWhite='\033[4;37m'       # White
+    spin='-\|/'
+    yellow=$(tput setaf 5)
+    normal=$(tput sgr0)
+    green=$(tput setaf 2)
+    width_checkmark=10
+    width_text=30
 
-# Background
-On_Black='\033[40m'       # Black
-On_Red='\033[41m'         # Red
-On_Green='\033[42m'       # Green
-On_Yellow='\033[43m'      # Yellow
-On_Blue='\033[44m'        # Blue
-On_Purple='\033[45m'      # Purple
-On_Cyan='\033[46m'        # Cyan
-On_White='\033[47m'       # White
+    checkmark="\xE2\x9C\x94"
+    green_checkmark="\t[${green}$checkmark${normal}]\n"
 
-# High Intensity
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-
-# Bold High Intensity
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
-
-# High Intensity backgrounds
-On_IBlack='\033[0;100m'   # Black
-On_IRed='\033[0;101m'     # Red
-On_IGreen='\033[0;102m'   # Green
-On_IYellow='\033[0;103m'  # Yellow
-On_IBlue='\033[0;104m'    # Blue
-On_IPurple='\033[0;105m'  # Purple
-On_ICyan='\033[0;106m'    # Cyan
-On_IWhite='\033[0;107m'   # White
+    i=0
+    printf "%-${width_text}b" "  - $1"
+    local pid=$!
+    local delay=0.075
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf "  [${yellow}%c${normal}]" "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b"
+    done
+    printf "  \b\b\b\b%${width_checkmark}b" "$green_checkmark"
+}
 
 # ------
+center_print "Setting up the IoT Box"
 ssh-keygen -f "/home/odoo/.ssh/known_hosts" -R ${ip}
 ssh-keyscan -H ${ip} >>~/.ssh/known_hosts
 
 sshpass -p "raspberry" ssh pi@${ip} 'sudo killall python3'
-echo "Open writing on IoT Box"
 sshpass -p "raspberry" ssh pi@${ip} 'sudo mount -o remount,rw /'
-echo "---"
 # ------
-echo "Send file to IoT Box in ${1}"
-echo "---"
+center_print "Sending files to" "${1}"
 sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_posbox_homepage/controllers/main.py pi@${ip}:/home/pi/odoo/addons/hw_posbox_homepage/controllers/
-echo "Sent addons/hw_posbox_homepage/controllers/main.py"
-echo "---"
-sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_drivers/controllers/* pi@${ip}:/home/pi/odoo/addons/hw_drivers/
-echo -e "${Cyan}Sent addons/hw_drivers/*${ENDCOLOR}"
-echo "---"
+sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_posbox_homepage/views/* pi@${ip}:/home/pi/odoo/addons/hw_posbox_homepage/views/ 2>/dev/null &
+loading "addons/hw_posbox_homepage/"
 sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_drivers/controllers/* pi@${ip}:/home/pi/odoo/addons/hw_drivers/controllers/
-echo "Sent addons/hw_drivers/controllers/*"
-echo "---"
+sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_drivers/iot_handlers/drivers/* pi@${ip}:/home/pi/odoo/addons/hw_drivers/iot_handlers/drivers/
 sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_drivers/tools/helpers.py pi@${ip}:/home/pi/odoo/addons/hw_drivers/tools/
-echo "Sent addons/hw_drivers/tools/helpers.py"
-echo "---"
 sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_drivers/iot_handlers/drivers/* pi@${ip}:/home/pi/odoo/addons/hw_drivers/iot_handlers/drivers/
-sshpass -p "raspberry" scp ${ENTERPRISE_DIR}/iot/iot_handlers/drivers/* pi@${ip}:/home/pi/odoo/addons/hw_drivers/iot_handlers/drivers/
-sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_drivers/iot_handlers/drivers/* pi@${ip}:/home/pi/odoo/addons/hw_drivers/iot_handlers/drivers/
-sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_drivers/iot_handlers/interfaces/* pi@${ip}:/home/pi/odoo/addons/hw_drivers/iot_handlers/interfaces/
-echo "Sent addons/hw_drivers/drivers/*"
-echo "---"
-sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_posbox_homepage/views/* pi@${ip}:/home/pi/odoo/addons/hw_posbox_homepage/views/
-echo "Sent addons/hw_posbox_homepage/views"
-echo "---"
-sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/point_of_sale/tools/posbox/configuration/* pi@${ip}:/home/pi/odoo/addons/point_of_sale/tools/posbox/configuration/
-echo "Sent addons/point_of_sale/tools/posbox/configuration/"
+sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/hw_drivers/iot_handlers/interfaces/* pi@${ip}:/home/pi/odoo/addons/hw_drivers/iot_handlers/interfaces/ 2>/dev/null &
+loading "addons/hw_drivers"
+sshpass -p "raspberry" scp ${ODOO_ADDONS_DIR}/point_of_sale/tools/posbox/configuration/* pi@${ip}:/home/pi/odoo/addons/point_of_sale/tools/posbox/configuration/ 2>/dev/null &
+loading "posbox/configuration/"
 
 # Manual second argument stuff
 if [ -z "${manu}" ] ; then
 echo "Restart Odoo server"
-echo "---"
 sshpass -p "raspberry" ssh pi@${ip} 'sudo service odoo restart'
 elif [ "${manu}" = "scp" ] ; then
 echo "Don't start Odoo server"
-echo "---"
 elif [ "${manu}" = "reboot" ] ; then
 sshpass -p "raspberry" ssh pi@${ip} 'sudo reboot'
 elif [ "${manu}" = "manual" ] ; then
 echo "Manual starting of Odoo"
-echo "---"
 sshpass -p "raspberry" ssh pi@${ip} 'sudo service odoo stop'
 sshpass -p "raspberry" ssh pi@${ip} 'odoo/./odoo-bin --load=web,hw_proxy,hw_posbox_homepage,hw_escpos,hw_drivers --limit-time-cpu=600 --limit-time-real=1200 --max-cron-threads=0' 
 fi
